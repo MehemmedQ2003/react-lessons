ca5cdcc31647aed10669ec62eb1c3afa1a316c4c
mehemmed qardasov <116949722+MehemmedQ2003@users.noreply.github.com>
2024-11-15 19:24:53 +0400

    What is state

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,17 @@ function App() {
         <div className='card-group w-100'>
           <div className='col p-3'>
             <Collapse href="collapseExample1">
-              <Card text="Card Text 1" button="Click me 1" image="https://picsum.photos/id/1/200/300" />
+              <Card text="Card Text 1" button="Click me 1" image="https://picsum.photos/id/1/200/200" />
             </Collapse>
           </div>
           <div className='col p-3'>
             <Collapse href="collapseExample2">
-              <Card title="Title 2" text="Card Text 2" button="Click me 2" image="https://picsum.photos/id/2/200/300" />
+              <Card title="Title 2" text="Card Text 2" button="Click me 2" image="https://picsum.photos/id/2/200/200" />
             </Collapse>
           </div>
           <div className='col p-3'>
             <Collapse href="collapseExample3">
-              <Card title="title 3" text="Card Text 3" button="Click me 3" image="https://picsum.photos/id/3/200/300" />
+              <Card title="title 3" text="Card Text 3" button="Click me 3" image="https://picsum.photos/id/3/200/200" />
             </Collapse>
           </div>
         </div>
